0e403088cd23d68056f594a6601f3b33d63605bc
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2021-09-05 22:14:02 +0800

    Update Kybook.js

--- Kybook.js
+++ Kybook.js
@@ -1,8 +1,8 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 
-obj.subscription.expires = 1661973290000;
-obj.subscription.skid = yar.premium.yearly;
-obj.subscription.purchased = 1630437290000;
+obj.expires = 1661973290000;
+obj.skid = yar.premium.yearly;
+obj.purchased = 1630437290000;
 body = JSON.stringify(obj);
 $done({body});
