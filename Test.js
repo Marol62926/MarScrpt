766c83968b3b30994936dedddb7eae0c9d34dbbd
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-05 17:11:07 +0800

    Update Test.js

--- Test.js
+++ Test.js
@@ -2,11 +2,20 @@ var body = $response.body; // 声明一个变量body并以响应消息体赋值
 var obj = JSON.parse(body); // JSON.parse()将json形式的body转变成对象处理
 
 obj = {
-     "data" : {
-          "vip" : true,
-          "vip_remain_days" : 365,
-     }
-};
+  "status" : 0,
+  "data" : {
+    "vipUserProductInfo" : {
+      "trailStatus" : 0,
+      "expireTime" : 1714355702000,
+      "isInExpireRenewDuration" : 0,
+      "vip" : true,
+      "subscribeType" : 1
+    },
+    "uidSubscribeType" : 1
+  },
+  
+}
+
 
 body = JSON.stringify(obj); // 重新打包回json字符串
 $done(body); // 结束修改
