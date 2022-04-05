d7fb2e0000d71388bb49aaa29f03709a5bd6cc85
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-05 23:39:36 +0800

    Update and rename Test.js to test.js

---
+++ test.js
@@ -0,0 +1,26 @@
+var body = $response.body; // 声明一个变量body并以响应消息体赋值
+var obj = JSON.parse(body); // JSON.parse()将json形式的body转变成对象处理
+
+obj = {
+  "superMonthlyExpire" : 1675608390000,
+  "newUserWelfareText" : "",
+  "isSVIPUser" : true,
+  "isNewUserVip" : false,
+  "balance" : 0,
+  "ok" : true,
+  "beanVoucherCount" : 0,
+  "isMonthly" : false,
+  "vipStatus" : 1,
+  "isAppstoreAutoMonthly" : false,
+  "voucherBalance" : 0,
+  "monthly" : 0,
+  "beanVoucherBalance" : 0,
+  "expiresDay" : 1675608390000,
+  "iosBalance" : 0,
+  "voucherCount" : 0
+}
+
+
+
+body = JSON.stringify(obj); // 重新打包回json字符串
+$done(body); // 结束修改
