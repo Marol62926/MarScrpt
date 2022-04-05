ebea0d519b0a10962f6ec190abd4e075c0d9f950
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-05 17:34:25 +0800

    Update and rename Test.js to test.js

--- test.js
+++ test.js
@@ -2,16 +2,11 @@ var body = $response.body; // 声明一个变量body并以响应消息体赋值
 var obj = JSON.parse(body); // JSON.parse()将json形式的body转变成对象处理
 
 obj = {
-  "status" : 0,
   "data" : {
     "vipUserProductInfo" : {
-      "trailStatus" : 0,
-      "expireTime" : 1714355702000,
-      "isInExpireRenewDuration" : 0,
+      "expireTime" : 1999999999999,
       "vip" : true,
-      "subscribeType" : 1
     },
-    "uidSubscribeType" : 1
   },
   
 }
