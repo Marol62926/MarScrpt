273d5051a0db44e5f7e4f976b552da4553281ba9
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-05 16:41:07 +0800

    Create Test.js

---
+++ Test.js
@@ -0,0 +1,12 @@
+var body = $response.body; // 声明一个变量body并以响应消息体赋值
+var obj = JSON.parse(body); // JSON.parse()将json形式的body转变成对象处理
+
+obj = {
+     "data" : {
+          "vip" : true,
+          "vip_remain_days" : 365,
+     }
+};
+
+body = JSON.stringify(obj); // 重新打包回json字符串
+$done(body); // 结束修改
