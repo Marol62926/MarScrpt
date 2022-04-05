dfcadb06881da7ef79080f463ac2ba77c4ea46a4
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-06 02:28:44 +0800

    Update and rename Ar.js to ar.js

--- ar.js
+++ ar.js
@@ -1,8 +1,10 @@
 var body = $response.body;
 var obj = JSON.parse(body); 
 
-var obj = {
-  "account_type":"2"
+var obj = ｛
+  "user" : {
+      "account_type":"2"
+    }
 };
 body = JSON.stringify(obj);
 $done({body});
