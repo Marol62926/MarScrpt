4953556ff48a348c4c025e1bf8fbaeda8f8ff255
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-06 20:05:10 +0800

    Update ky.js

--- ky.js
+++ ky.js
@@ -2,16 +2,31 @@ var body = $response.body; // 声明一个变量body并以响应消息体赋值
 var obj = JSON.parse(body); // JSON.parse()将json形式的body转变成对象处理
 
 obj = {
-  "userid" : "L7RRiRx",
-  "good" : [
+  "subscriptions" : [
     {
-      "expires" : 1675615964,
-      "product_id" : "yar.premium.yearly",
-      "purchased" : 1630849942
+      "renewsAt" : "2023-03-28T20:58:25.000Z",
+      "purchaserId" : null,
+      "legacySubscriptionId" : "HSSUBS_B2CSUBSCRIBER",
+      "tags" : [
+        "CURRENT_SUB"
+      ],
+      "membership" : "PRIMARY",
+      "platform" : "GOOGLE",
+      "endsAt" : "2023-03-28T20:58:25.000Z",
+      "renewalTerm" : "ANNUAL",
+      "subscriptionState" : "PAYING",
+      "plan" : {
+        "subscriptionType" : "B2C",
+        "affiliation" : null,
+        "initialTermMonths" : 12,
+        "name" : "Android Annual (14-day free trial)"
+      },
+      "voucher" : {
+        "code" : "andiap12m_14dayfreetrial"
+      },
+      "startsAt" : "2021-04-14T18:58:27.000Z",
+      "status" : "ACTIVE"
     }
-  ],
-  "fail" : [
-
   ]
 }
 
