8f52ee40723f444fe9da1ba668f2b67da5f53b0f
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-06 20:27:09 +0800

    Update ar.js

--- ar.js
+++ ar.js
@@ -1,10 +1,24 @@
 var body = $response.body;
 var obj = JSON.parse(body); 
 
-var obj = ｛
-  "user" : {
-      "account_type":"2"
-    }
+var obj = {
+  "firstName" : "Marol",
+  "creationDate" : "2022-04-06T12:11:05.000+00:00",
+  "isPhiProtected" : false,
+  "hsId" : "HSUSER_XWR1ZZZ0K2KG941Q4",
+  "email" : "fukr81@163.com",
+  "countryCode" : "US",
+  "connections" : [
+   
+  ],
+  "privileges" : [
+    "STANDARD_CONTENT",
+    "SUBSCRIBER",
+    "V1_CONTENT"
+  ],
+  "stateCode" : null,
+  "lastName" : "Lee"
 };
+
 body = JSON.stringify(obj);
 $done({body});
