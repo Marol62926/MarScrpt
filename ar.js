3963cbe83798a5f494b9e6597393152b39e4f02f
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-09 17:05:34 +0800

    Update ar.js

--- ar.js
+++ ar.js
@@ -6,7 +6,7 @@ var obj = {"balance":0,"iosBalance":0,"voucherBalance":0,
            "voucherCount":0,"beanVoucherCount":0,"isNewUser":false,
            "time":1649493057,"voucherSum":0,"isFree":true,"freeTime":1649493057,
            "isAppstoreAutoMonthly":true,"isNewUserVip":true,"newUserWelfareText":"",
-           "vipId":"","vipStatus":1,"expiresDay":123,"superMonthlyExpire":0,"isSVIPUser":true,"ok":true}
+           "vipId":"","vipStatus":1,"expiresDay":1678352707,"superMonthlyExpire":1678352707,"isSVIPUser":true,"ok":true}
 
 
 body = JSON.stringify(obj);
