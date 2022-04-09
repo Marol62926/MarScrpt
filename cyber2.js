2331f825cf21abf8c4ca5afb019be11b2705b65c
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-09 19:13:31 +0800

    Create cyber2.js

---
+++ cyber2.js
@@ -0,0 +1,10 @@
+{
+  "product_id" : 40179,
+  "status" : 1,
+  "isrestore" : true,
+  "price" : 49.990000000000002,
+  "environment" : "Production",
+  "subscription_id" : 8346984,
+  "oauth_token" : "132169910_547bdb6cd3dddd6c3adaaa6c1dacb1a02f5fb80e",
+  "product_name" : "iOS Plan - Yearly Offer"
+}
