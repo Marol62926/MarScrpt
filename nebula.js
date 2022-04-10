79d3060b0e4becaf3a3b6ee5bc75c912bf573497
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-10 12:15:54 +0800

    Create nebula.js

---
+++ nebula.js
@@ -0,0 +1,32 @@
+##/*
+#圈X
+#[rewrite_local]
+#NEbula解锁vip, 星座运势
+#https://zh-api.nebulahoroscope.com/api/v7/users/*/premium url script-response-body https://raw.githubusercontent.com/Marol62926/MarScrpt/main/imuseum.js
+#[mitm]
+#hostname = zh-api.nebulahoroscope.com
+#*/
+
+var body = $response.body;
+var obj = JSON.parse(body);
+
+obj = {
+  "data": {
+    "premium": true,
+    "is_lifetime": true,
+    "retry_period": true,
+    "trial": false,
+    "sandbox": true,
+    "web": true,
+    "purchase_name": null,
+    "source": null,
+    "expired_at": null,
+    "is_need_show_winback": true
+  },
+  "error": [],
+  "result": true
+}
+
+
+body = JSON.stringify(obj); 
+$done(body); 
