bb8ef4d08fa6fde10ce3c85bb188a749677c5d90
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-12 21:09:26 +0800

    Update widgetsmith.js

--- widgetsmith.js
+++ widgetsmith.js
@@ -1,24 +1,40 @@
-let obj = JSON.parse($response.body);
+var body = $response.body; 
+var obj = JSON.parse(body); 
 
-obj.subscriber.entitlements = {
-      "Premium":{
-              "expires_date":"2029-05-26T05:05:04Z",
-              "product_identifier":"PremiumMonthly",
-              "purchase_date":"2022-04-09T05:05:04Z"
+obj = {
+  "subscriber": {
+    "entitlements": {
+      "Premium": {
+        "expires_date":"2029-05-26T05:05:04Z",
+        "grace_period_expires_date": null,
+        "product_identifier": "PremiumMonthly",
+        "purchase_date": "2022-04-12T12:54:03Z"
       }
-  },
-  
-obj.subscriber.subscriptions ={
-      "PremiumMonthly":{
-              "billing_issues_detected_at":null,
-              "expires_date":"2029-05-26T05:05:04Z",
-              "is_sandbox":false,
-              "original_purchase_date":"2022-04-09T05:05:04Z",
-              "period_type":"trial",
-              "purchase_date":"2022-04-09T05:05:04Z",
-              "store":"app_store",
-              "unsubscribe_detected_at":null
+    },
+    "first_seen": "2022-04-12T12:51:37Z",
+    "last_seen": "2022-04-12T12:51:37Z",
+    "management_url": "https://apps.apple.com/account/subscriptions",
+    "non_subscriptions": {},
+    "original_app_user_id": "",
+    "original_application_version": "114",
+    "original_purchase_date": "2022-04-12T10:06:40Z",
+    "other_purchases": {},
+    "subscriptions": {
+      "PremiumMonthly": {
+        "billing_issues_detected_at": null,
+        "expires_date":"2029-05-26T05:05:04Z",
+        "grace_period_expires_date": null,
+        "is_sandbox": false,
+        "original_purchase_date": "2022-04-12T12:54:03Z",
+        "ownership_type": "PURCHASED",
+        "period_type": "trial",
+        "purchase_date": "2022-04-12T12:54:03Z",
+        "store": "app_store",
+        "unsubscribe_detected_at": null
       }
+    }
   }
+}
 
-$done({body: JSON.stringify(obj)});
+body = JSON.stringify(obj);
+$done(body);
