eadea865b03a4b057ed2ba2b161a989698240068
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-12 20:18:46 +0800

    Update shadowlink.js

--- shadowlink.js
+++ shadowlink.js
@@ -30,7 +30,7 @@ obj = {
     "original_application_version": "22",
     "in_app": [{
       "quantity": "1",
-      "product_id": "com.shadowlink.subscription.year",
+      "product_id": "com.shadowlink.subscriptionnn.year",
       "transaction_id": "730000766600650",
       "original_transaction_id": "730000766600650",
       "purchase_date": "2021-11-25 17:07:45 Etc/GMT",
@@ -50,7 +50,7 @@ obj = {
   },
   "latest_receipt_info": [{
     "quantity": "1",
-    "product_id": "com.shadowlink.subscription.year",
+    "product_id": "com.shadowlink.subscriptionnn.year",
     "transaction_id": "730000766600650",
     "original_transaction_id": "730000766600650",
     "purchase_date": "2021-11-25 17:07:45 Etc/GMT",
@@ -70,8 +70,8 @@ obj = {
   }],
   "latest_receipt": "",
   "pending_renewal_info": [{
-    "auto_renew_product_id": "com.shadowlink.subscription.year",
-    "product_id": "com.shadowlink.subscription.year",
+    "auto_renew_product_id": "com.shadowlink.subscriptionnn.year",
+    "product_id": "com.shadowlink.subscriptionnn.year",
     "original_transaction_id": "730000766600650",
     "auto_renew_status": "1"
   }],
