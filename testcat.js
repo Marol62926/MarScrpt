32054b7c9867274de70a7371004564fb3316e7cd
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-12 23:21:32 +0800

    Create testcat.js

---
+++ testcat.js
@@ -0,0 +1,24 @@
+let obj = JSON.parse($response.body);
+
+obj.subscriber.entitlements = {
+      "pro":{
+              "expires_date":"2029-05-26T05:05:04Z",
+              "product_identifier":"com.rk.horoscope.month12.full",
+              "purchase_date":"2022-04-09T05:05:04Z"
+      }
+  },
+  
+obj.subscriber.subscriptions ={
+      "com.rk.horoscope.month12.full":{
+              "billing_issues_detected_at":null,
+              "expires_date":"2029-05-26T05:05:04Z",
+              "is_sandbox":false,
+              "original_purchase_date":"2022-04-09T05:05:04Z",
+              "period_type":"trial",
+              "purchase_date":"2022-04-09T05:05:04Z",
+              "store":"app_store",
+              "unsubscribe_detected_at":null
+      }
+  }
+
+$done({body: JSON.stringify(obj)});
