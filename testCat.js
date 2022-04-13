b91febcc3212a7965ee606a27925927ab26154f8
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-13 11:20:56 +0800

    Update and rename testcat.js to testCat.js

--- testCat.js
+++ testCat.js
@@ -4,13 +4,13 @@ var obj = JSON.parse(body);
 obj.subscriber.entitlements = {
       "membership":{
               "expires_date":"2029-05-26T05:05:04Z",
-              "product_identifier":"vsco_global_2999_annual_7D_free",
+              "product_identifier":"vsco_global_2999_annual",
               "purchase_date":"2022-04-09T05:05:04Z"
       }
   },
   
 obj.subscriber.subscriptions ={
-      "vsco_global_2999_annual_7D_free":{
+      "vsco_global_2999_annual":{
               "billing_issues_detected_at":null,
               "expires_date":"2029-05-26T05:05:04Z",
               "is_sandbox":false,
