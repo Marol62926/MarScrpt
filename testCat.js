ff049211f4e31faefd9b353c13a530c00fdc6c97
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-13 14:18:46 +0800

    Update testCat.js

--- testCat.js
+++ testCat.js
@@ -4,13 +4,13 @@ var obj = JSON.parse(body);
 obj.subscriber.entitlements = {
       "membership":{
               "expires_date":"2029-05-26T05:05:04Z",
-              "product_identifier":"vsco_global_2999_annual",
+              "product_identifier":"PremiumMonthly",
               "purchase_date":"2022-04-09T05:05:04Z"
       }
   },
   
 obj.subscriber.subscriptions ={
-      "vsco_global_2999_annual":{
+      "PremiumMonthly":{
               "billing_issues_detected_at":null,
               "expires_date":"2029-05-26T05:05:04Z",
               "is_sandbox":false,
