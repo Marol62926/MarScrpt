f31ad50ff3cc0b30ff90e2233b59ca84c09ac84b
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-13 15:23:43 +0800

    Update and rename vsco.js to Vsco.js

--- Vsco.js
+++ Vsco.js
@@ -4,13 +4,13 @@ var obj = JSON.parse(body);
 obj.subscriber.entitlements = {
       "membership":{
               "expires_date":"2029-05-26T05:05:04Z",
-              "product_identifier":"vsco_global_2999_annual",
+              "product_identifier":"vsco_global_2999_annual_7D_free",
               "purchase_date":"2022-04-09T05:05:04Z"
       }
   },
   
 obj.subscriber.subscriptions ={
-      "vsco_global_2999_annual":{
+      "vsco_global_2999_annual_7D_free":{
               "billing_issues_detected_at":null,
               "expires_date":"2029-05-26T05:05:04Z",
               "is_sandbox":false,
