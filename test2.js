58c822a20020db49382deacdc01c952f55354895
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-13 17:41:12 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -3,24 +3,26 @@ var obj = JSON.parse(body);
 
 obj = {
   "status": "success",
+  "timestamp": 1649841369,
   "data": {
+    "id": 7791101,
     "balance": 10000,
     "is_vip": 1,
     "is_av_vip": 1,
-    "cover_frame_url": "",
+    "cover_frame_url": "http:\/\/chinanetcenter.pic.akemanga.com\/images\/project\/avatar_frame\/dfc6e11430e6187e35edb3c1fa0f1eec.png?format=webp",
     "vip_expire_time": 1870766006,
     "av_vip_expire_time": 1870766006,
-    "month_card_expire_time": 1870766006,
+    "month_card_expire_time": 0,
     "signin": true,
     "signin_rice": 1,
     "is_anonymous": 0,
     "facebook_id": null,
-    "nickname": ,
+    "nickname": 7791101,
     "image_url": "",
     "bg_image_url": "",
     "intro": "",
     "return_mili": 0,
-    "level": 5,
+    "level": 0,
     "gender": -1,
     "birthday": "",
     "open_id": "",
@@ -36,6 +38,7 @@ obj = {
     },
     "readcoupon": 0,
     "showfun_info": {
+      "id": "8663966",
       "image_url": "",
       "name": "用戶",
       "gender": 0,
@@ -44,7 +47,6 @@ obj = {
     }
   }
 }
-
 
 
 body = JSON.stringify(obj);
