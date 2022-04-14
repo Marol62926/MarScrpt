743974cf64e93deea1baa317ccb334b8ca7cfb6c
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-14 14:53:53 +0800

    Update test.js

--- test.js
+++ test.js
@@ -22,7 +22,7 @@ obj = {
     "original_application_version": "22",
     "in_app": [{
       "quantity": "1",
-      "product_id": "com.photoscanners.yearly",
+      "product_id": "",
       "transaction_id": "730000766600650",
       "original_transaction_id": "730000766600650",
       "purchase_date": "2021-11-25 17:07:45 Etc/GMT",
@@ -42,7 +42,7 @@ obj = {
   },
   "latest_receipt_info": [{
     "quantity": "1",
-    "product_id": "com.photoscanners.yearly",
+    "product_id": "",
     "transaction_id": "730000766600650",
     "original_transaction_id": "730000766600650",
     "purchase_date": "2021-11-25 17:07:45 Etc/GMT",
@@ -62,8 +62,8 @@ obj = {
   }],
   "latest_receipt": "",
   "pending_renewal_info": [{
-    "auto_renew_product_id": "com.photoscanners.yearly",
-    "product_id": "com.photoscanners.yearly",
+    "auto_renew_product_id": "",
+    "product_id": "",
     "original_transaction_id": "730000766600650",
     "auto_renew_status": "1"
   }],
