a481dc1d1ea2048b0064b37b71433d02f1c95560
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-15 13:17:17 +0800

    Update Vsco.js

--- Vsco.js
+++ Vsco.js
@@ -1,26 +1,14 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 
-obj.subscriber.entitlements = {
-      "membership":{
-              "expires_date":"2029-05-26T05:05:04Z",
-              "product_identifier":"vsco_global_2999_annual_7D_free",
-              "purchase_date":"2022-04-09T05:05:04Z"
-      }
-  },
-  
-obj.subscriber.subscriptions ={
-      "vsco_global_2999_annual_7D_free":{
-              "billing_issues_detected_at":null,
-              "expires_date":"2029-05-26T05:05:04Z",
-              "is_sandbox":false,
-              "original_purchase_date":"2022-04-09T05:05:04Z",
-              "period_type":"trial",
-              "purchase_date":"2023-04-09T05:05:04Z",
-              "store":"app_store",
-              "unsubscribe_detected_at":null
-      }
-  }
+obj = {
+      "isvip": 1,
+      "vipStatus": 1,
+      "vipEndDate": "2029-01-01",
+      "vipEndDateTime": "2029-01-01 10:19:38",
+      "vipDays": 365,
+      "level": 5,
+}
 
 body = JSON.stringify(obj); 
 $done(body); 
