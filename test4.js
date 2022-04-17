210c50e360fe53ee76c322cdec2c5b69732e31c6
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-17 12:21:24 +0800

    Update test4.js

--- test4.js
+++ test4.js
@@ -3,8 +3,7 @@ var obj = JSON.parse(body);
 
 obj.result.now_vip.is_forever = true,
 obj.result.now_vip.is_vip = true,  
-obj.result.now_vip.expire_time = 1871093782,
-obj.result.now_vip.expire_notice = "",  
+obj.result.now_vip.expire_time = 1871093782,  
   
 obj.result.yoga.is_vip = true,  
 obj.result.yoga.expire_time = 1871093782,
