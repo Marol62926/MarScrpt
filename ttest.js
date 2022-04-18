68e0d19aec908da3aa12d3b4e49c16a34bd01b2d
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-18 13:14:12 +0800

    Update ttest.js

--- ttest.js
+++ ttest.js
@@ -6,7 +6,7 @@ obj = {
     "receipt_type": "Production",
     "adam_id": 1569291816,
     "app_item_id": 1569291816,
-    "bundle_id": "com.morpholioapps.trace",
+    "bundle_id": "com.firecore.infuse",
     "application_version": "40",
     "download_id": 500935934584949171,
     "version_external_identifier": 846821007,
