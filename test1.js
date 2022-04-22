0b7b207619d040aafd675e58793d7bca4a97fe17
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-22 14:09:18 +0800

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,7 +1,12 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 
-obj.vip_type = 2,
+obj.user.subscription = {
+      "offer": "sublime+",
+      "periodicity": "annual",
+      "end_date": "2023-02-05",
+      "is_canceled": false
+    },
 
 body = JSON.stringify(obj); 
 $done(body); 
