d13f95f9e00840b0eed3fbd9a84ad3294e779067
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-23 23:25:48 +0800

    Update testcat.js

--- testcat.js
+++ testcat.js
@@ -2,15 +2,15 @@ var body = $response.body;
 var obj = JSON.parse(body);
 
 obj.subscriber.entitlements = {
-      "sub.pro":{
+      "Premium":{
               "expires_date":"2029-05-26T05:05:04Z",
-              "product_identifier":"com.pspdfkit.viewer.sub.pro.yearly",
+              "product_identifier":"com.monkeytaps.iam.premium.year",
               "purchase_date":"2022-04-09T05:05:04Z"
       }
   },
   
 obj.subscriber.subscriptions ={
-      "com.pspdfkit.viewer.sub.pro.yearly":{
+      "com.monkeytaps.iam.premium.year":{
               "billing_issues_detected_at":null,
               "expires_date":"2029-05-26T05:05:04Z",
               "is_sandbox":false,
