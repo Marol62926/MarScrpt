f3d1fa150414196b3900fdbd827f389c82350907
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-24 22:22:03 +0800

    Update obj.js

--- obj.js
+++ obj.js
@@ -1,7 +1,22 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 
-obj.result.account.account_type = "unlimited",
+obj = {
+  "isValid": true,
+  "autoRenewalStatus": null,
+  "productId": "com.accuweather.annual.subscription",
+  "expiryDateEpoch": 1871734859,
+  "expirationDate": "2029-05-01T14:18:35+00:00",
+  "expirationIntent": null,
+  "expirationRetry": null,
+  "purchaseDateEpoch": 1650809915000,
+  "isTrial": false,
+  "isIntro": true,
+  "status": 0,
+  "usedTrial": ["com.accuweather.annual.subscription"],
+  "usedIntro": null,
+  "isRetryable": null
+}
   
 
 body = JSON.stringify(obj); 
