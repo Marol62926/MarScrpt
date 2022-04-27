b1917a7ec894804a8441021f13e76c9a045be7b3
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-04-28 01:30:28 +0800

    Update and rename test.js to tenPercent.js

--- tenPercent.js
+++ tenPercent.js
@@ -5,6 +5,7 @@ obj.user.expiration_date = "05/05/2029, 05:13:20 PM UTC",
 obj.user.plan_description = "1 year",
 obj.user.subscription_end_date = "05/04/2029, 05:13:20 PM UTC",
 obj.user.subscription_period = "P1Y",
+obj.user.subscription_source = "62926",
 
 body = JSON.stringify(obj); 
 $done(body); 
