aa87111c4f8bd4091fbd61ce6aff8c688a00a023
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-05-02 18:26:24 +0800

    Create flo.js

---
+++ flo.js
@@ -0,0 +1,11 @@
+var body = $response.body; 
+var obj = JSON.parse(body); 
+
+obj = {
+  "status": "active",
+  "service_level": 1,
+  "auto_renew_status": true,
+}
+
+body = JSON.stringify(obj);
+$done(body);
