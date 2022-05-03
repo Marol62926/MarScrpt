88134c8422ff35c2dd0a5a41caa93c2485660849
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-05-03 22:48:38 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,12 +1,9 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 
-obj = {
-  "ok": 1,
-  "valid": 1,
-  "valid_from": 1651564385,
-  "valid_to": 1652169184
-}
+obj.data.isPremium = true,
+obj.data.showSubscriptionProducts = false,
+obj.data.userType = "premium yearly"
 
 body = JSON.stringify(obj); 
 $done(body); 
