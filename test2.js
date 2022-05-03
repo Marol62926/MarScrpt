1a321f64b3f24372012481245693ade7674ff326
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-05-03 23:07:42 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,9 +1,16 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 
-obj.data.isPremium = true,
-obj.data.showSubscriptionProducts = false,
-obj.data.userType = "premium yearly"
+obj.subscription = [{
+    "originalTransactionID": "",
+    "productID": "droom.sleepIfUCanFree.premium.yearly01.4",
+    "willAutoRenew": true,
+    "isActive": true,
+    "expiresDateMs": 1883142423000,
+    "periodType": "TRIAL",
+    "latestPurchaseDateMs": 1651590191000,
+    "originalPurchaseDateMs": 1651590191000
+  }],
 
 body = JSON.stringify(obj); 
 $done(body); 
