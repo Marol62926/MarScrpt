f5b06e281659211e091728208ac3848f92ac57c7
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-05-05 19:22:33 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,30 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 
-obj = '"])}while(1);</x>//{"A":{"id":"27","name":"Canva Pro","descriptor":"CANVA_FOR_WORK","description":"August 2021 prices","featureBundle":"PAID","trialPeriodDays":14,"trialsPerBrand":2,"subscriberType":"BRAND","planProductConfig":{"A?":"A","A":"PAID"},"legacy":false,"userLocked":false,"free":false,"graceEnabled":true,"flexibleBillingEnabled":false,"rollupBillingEnabled":false}}
-
+obj = {
+  "subscriptions": [{
+    "subscription_name": "AcrobatPlus",
+    "subscription_level": "Plus",
+    "subscription_state": "ACTIVE",
+    "sub_ref": "",
+    "biz_source": "CCC",
+    "billing_term": null
+  }, {
+    "subscription_name": "ESign",
+    "subscription_level": "Basic",
+    "subscription_state": "ACTIVE",
+    "sub_ref": "",
+    "biz_source": "CCC",
+    "billing_term": null
+  }, {
+    "subscription_name": "CreatePDF",
+    "subscription_level": "Basic",
+    "subscription_state": "ACTIVE",
+    "sub_ref": "",
+    "biz_source": "CCC",
+    "billing_term": null
+  }]
+}
+  
 body = JSON.stringify(obj); 
 $done(body); 
