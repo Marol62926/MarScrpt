234d6a126c48334170e6676c5ca56e7d94f938d0
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-05-05 23:47:23 +0800

    Update and rename adobeScanPdf.js to AdobeScanPdf.js

--- AdobeScanPdf.js
+++ AdobeScanPdf.js
@@ -6,21 +6,28 @@ obj = {
     "subscription_name": "AcrobatPlus",
     "subscription_level": "Plus",
     "subscription_state": "ACTIVE",
-    "sub_ref": "F9321292BD8EBB630BBA",
+    "sub_ref": "",
     "biz_source": "CCC",
     "billing_term": null
   }, {
     "subscription_name": "ESign",
     "subscription_level": "Basic",
     "subscription_state": "ACTIVE",
-    "sub_ref": "F9321292BD8EBB630BBA",
+    "sub_ref": "",
     "biz_source": "CCC",
+    "billing_term": null
+  }, {
+    "subscription_name": "PDFPack",
+    "subscription_level": "Plus",
+    "subscription_state": "ACTIVE",
+    "sub_ref": "",
+    "biz_source": "",
     "billing_term": null
   }, {
     "subscription_name": "CreatePDF",
     "subscription_level": "Basic",
     "subscription_state": "ACTIVE",
-    "sub_ref": "F9321292BD8EBB630BBA",
+    "sub_ref": "",
     "biz_source": "CCC",
     "billing_term": null
   }]
