7dbcd29afc70de6d6743eab97af5b12a49b32dba
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-05-05 22:34:17 +0800

    Delete ttest.js

--- ttest.js
+++
@@ -1,9 +0,0 @@
-var body = $response.body;
-var obj = JSON.parse(body);
-
-obj.mobileProfile.profileStatus = "PROFILE_AVAILABLE",
-obj.mobileProfile.legacyProfile = "{\"licenseId\":\"TEMP_LICENSE_PROD\",\"licenseType\":3,\"licenseVersion\":\"1.0\",\"effectiveEndTimestamp\":1651847307970,\"graceTime\":0,\"licensedFeatures\":[],\"enigmaData\":{\"productId\":598,\"serialKey\":\"719594172484074650698776\",\"clearSerialKey\":\"90970938077827684226\",\"locale\":\"ALL\",\"associatedLocales\":\"ALL\",\"platform\":0,\"isk\":5984027,\"customerId\":0,\"deliveryMethod\":3,\"pc\":true,\"rb\":true}}",
-obj.mobileProfile.relationshipProfile = "[{\"profileGenerationTimestamp\":1651760928176,\"licenseId\":\"TEMP_LICENSE_PROD\",\"licenseExpiryTimestamp\":1651847307970,\"appEntitlementStatus\":\"SUBSCRIPTION\",\"activationType\":\"NAMED_USER\",\"billingStatus\":\"NORMAL\",\"usedForLegacyProfile\":true,\"licenseExpiryWarningControl\":{\"warningStartTimestamp\":1651242507970,\"warningInterval\":0}}]",
-  
-body = JSON.stringify(obj); 
-$done(body); 
