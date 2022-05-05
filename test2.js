e9af197d7836487efdf03e67809038b1edc07dbf
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-05-05 18:27:49 +0800

    Update and rename test2.js to ttest2.js

--- test2.js
+++
@@ -1,8 +0,0 @@
-var body = $response.body;
-var obj = JSON.parse(body);
-
-obj.profileStatus = "PROFILE_AVAILABLE",
-obj.legacyProfile = "{\"licenseId\":\"B93D68DC4A03CDE0B90A\",\"licenseType\":3,\"licenseVersion\":\"1.0\",\"effectiveEndTimestamp\":1652597999000,\"graceTime\":8553600000,\"licensedFeatures\":[],\"enigmaData\":{\"productId\":598,\"serialKey\":\"909707769301275563002882\",\"clearSerialKey\":\"15984055698707407487\",\"locale\":\"ALL\",\"associatedLocales\":\"ALL\",\"platform\":0,\"isk\":5984027,\"customerId\":0,\"deliveryMethod\":3,\"pc\":true,\"rb\":true}}",
-obj.relationshipProfile = "[{\"profileGenerationTimestamp\":1651745145759,\"licenseId\":\"B93D68DC4A03CDE0B90A\",\"licenseExpiryTimestamp\":1661151599000,\"appEntitlementStatus\":\"SUBSCRIPTION\",\"activationType\":\"NAMED_USER\",\"billingStatus\":\"NORMAL\",\"usedForLegacyProfile\":true,\"licenseExpiryWarningControl\":{\"warningStartTimestamp\":1660546799000,\"warningInterval\":0}}]",
-body = JSON.stringify(obj); 
-$done(body); 
