28b6f8dc78795dafef4bed5ed85d456c2a4f463b
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-05-07 14:44:27 +0800

    Update and rename test.js to test1.js

--- test1.js
+++ test1.js
@@ -6,14 +6,14 @@ obj = {
   "remainCount" : 6666,
   "payingRemainTime" : 11116,
   "day" : 91,
-  "remainTime" : 99306196,
+  "remainTime" : 9930,
   "signature" : "80983bb9278bc8a7780a288b63fe5fe40994e945d0de99a4183dcef0fa17109d",
   "isAutoRenewable" : 1,
   "permanent" : 1,
   "expired" : 0,
   "historyAutoRenewable" : 1,
   "autoRenewableChannel" : 6,
-  "isPaying" : 1,
+  "isPaying" : 0,
   "autoRenewableTime" : 6,
   "totalFreeReadDay" : 6666,
   "expiredTime" : 1872518379,
@@ -23,14 +23,6 @@ obj = {
   "autoRenewablePrice" : 991900,
   "remainCoupon" : 6666,
   "startTime" : 1651904966,
-  "hintsForRecharge" : {
-    "buttonSubtitle" : "你正在使用免费无限卡 · 升级付费无限卡即可阅读",
-    "predictedSavedMoney" : 9910315,
-    "predictedChapterPrice" : 15,
-    "sendCoupons" : 6666,
-    "buttonTitle" : "付费无限卡9元/月 · 限时特惠",
-    "pricePerMonth" : 99900
-  }
 }
 
   
