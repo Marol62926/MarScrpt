9fae2b6e6dd53aa3b83923544e4135551f958e8a
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-05-08 21:44:09 +0800

    Update test1.js

--- test1.js
+++ test1.js
@@ -2,27 +2,18 @@ var body = $response.body;
 var obj = JSON.parse(body);
 
 obj = {
-  "random" : 9453,
-  "remainCount" : 6666,
-  "payingRemainTime" : 11116,
-  "day" : 91,
-  "remainTime" : 9930,
-  "signature" : "80983bb9278bc8a7780a288b63fe5fe40994e945d0de99a4183dcef0fa17109d",
-  "isAutoRenewable" : 1,
-  "permanent" : 1,
-  "expired" : 0,
-  "historyAutoRenewable" : 1,
-  "autoRenewableChannel" : 6,
-  "isPaying" : 0,
-  "autoRenewableTime" : 6,
-  "totalFreeReadDay" : 6666,
-  "expiredTime" : 1872518379,
-  "timestamp" : 1651904842,
-  "canUseDiscount" : 0,
-  "savedMoney" : 9927433,
-  "autoRenewablePrice" : 991900,
-  "remainCoupon" : 6666,
-  "startTime" : 1651904966,
+  "status" : "ACTIVE",
+  "canGetTrial" : false,
+  "startDate" : "2022-05-08T13:33:03.015+0000",
+  "subscription" : {
+    "type" : "PREMIUM_PLUS",
+    "offlineGracePeriod" : 30
+  },
+  "premiumAccess" : true,
+  "paymentType" : "ADYEN_CREDIT_CARD",
+  "paymentOverdue" : false,
+  "highestSoundQuality" : "HI_RES",
+  "validUntil" : "2022-06-07T13:33:01.686+0000"
 }
 
   
