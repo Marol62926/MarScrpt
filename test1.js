e61e785a7451925d1549609e9eb6c5822825b3c6
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-05-28 02:32:50 +0800

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,14 +1,34 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 
-obj.data.is_premium = 1,
-obj.data.type = "diamond",
-obj.data.sku = "diamond_yearly",
-obj.data.date": {
-      "start": 1644652800,
-      "last_renewed": 1644652800,
-      "expires": 1874599191
+obj = {
+  "status" : {
+    "message" : "OK",
+    "code" : 0
+  },
+  "result" : {
+    "id" : 576620529,
+    "state" : "trialing",
+    "limited_validity" : null,
+    "next_payment_due" : 1654885689,
+    "last_paid_date" : null,
+    "total_price" : {
+      "value" : 8.9900000000000002,
+      "currency" : "USD"
     },
+    "created_at" : 1653676089,
+    "can_be_canceled" : false,
+    "description" : "Monthly recurring membership",
+    "type" : "apple",
+    "title" : "Month Pass",
+    "purchase_valid_to" : null,
+    "can_be_renewed" : false,
+    "subscription" : true,
+    "subscription_free_trial_days" : 14,
+    "product_handle" : "com.scribd.premium.monthly.unlimited.14_day_trial",
+    "subscription_duration" : "1.month"
+  }
+}
   
 body = JSON.stringify(obj); 
 $done(body); 
