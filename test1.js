983e399643f22f48eb9ffa463cb1d7b26832bfda
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-05-28 02:00:09 +0800

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,21 +1,14 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 
-obj = {
-  "status" : "ACTIVE",
-  "canGetTrial" : false,
-  "startDate" : "2022-05-08T13:33:03.015+0000",
-  "subscription" : {
-    "type" : "PREMIUM_PLUS",
-    "offlineGracePeriod" : 30
-  },
-  "premiumAccess" : true,
-  "paymentType" : "ADYEN_CREDIT_CARD",
-  "paymentOverdue" : false,
-  "highestSoundQuality" : "HI_RES",
-  "validUntil" : "2022-06-07T13:33:01.686+0000"
-}
-
+obj.data.is_premium = 1,
+obj.data.type = "diamond",
+obj.data.sku = "diamond_yearly",
+obj.data.date": {
+      "start": 1644652800,
+      "last_renewed": 1644652800,
+      "expires": 1874599191
+    },
   
 body = JSON.stringify(obj); 
 $done(body); 
