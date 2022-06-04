446396205ea0bec03cdd799040bbe65d9c9b0d0a
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-06-04 14:24:13 +0800

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,28 +1,17 @@
-var body = $response.body;
-var obj = JSON.parse(body);
+name: ColorWidget
+desc: v3.1.1
 
-obj.result = {
-    "id" : 576620529,
-    "state" : "trialing",
-    "limited_validity" : null,
-    "next_payment_due" : 1654885689,
-    "last_paid_date" : null,
-    "total_price" : {
-      "value" : 8.9900000000000002,
-      "currency" : "USD"
-    },
-    "created_at" : 1653676089,
-    "can_be_canceled" : false,
-    "description" : "Monthly recurring membership",
-    "type" : "apple",
-    "title" : "Month Pass",
-    "purchase_valid_to" : null,
-    "can_be_renewed" : false,
-    "subscription" : true,
-    "subscription_free_trial_days" : 14,
-    "product_handle" : "com.scribd.premium.monthly.unlimited.14_day_trial",
-    "subscription_duration" : "1.month"
-  }
-  
-body = JSON.stringify(obj); 
-$done(body); 
+http:
+  mitm:
+    - "api.revenuecat.com"
+  script:
+    - match: https://api.revenuecat.com/v1/receipts
+      name: notability
+      type: response
+      require-body: true
+      timeout: 10
+      
+script-providers:
+  ColorWidget:
+    url: https://raw.githubusercontent.com/Marol62926/MarScrpt/main/colorwidget.js
+    interval:  86400
