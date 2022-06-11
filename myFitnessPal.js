f3abf363a976b05993401d089f2e310d159e2bd5
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-06-11 13:53:43 +0800

    Update myFitnessPal.js

--- myFitnessPal.js
+++ myFitnessPal.js
@@ -5,5 +5,5 @@ obj.subscriptionEndDate = "2029-05-01T10:10:53.929Z",
 obj.subscriptionStartDate = "2022-05-01T10:10:53.929Z",
 obj.hasPremium = true,
 
-body = JSON.stringify(obj); 
-$done(body); 
+body = JSON.stringify(obj);
+$done({body}); 
