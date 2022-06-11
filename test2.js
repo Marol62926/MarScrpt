8b2270ff82384b1c52ebac5ce2da68ea7cf1a1f8
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-06-12 02:28:16 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,29 +1,26 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 
-obj = {
-  "id": "3BF9DBFF-1C25-4BDA-AA50-368D2C4D7CAB",
-  "plan": "com.zerofasting.yearly_6999_7d_trial_2",
-  "store": "APP_STORE",
-  "type": "INITIAL_PURCHASE",
-  "startDate": "2022-06-11T17:36:03Z",
-  "expirationDate": "2029-06-18T17:36:03Z",
-  "eventDate": "2022-06-11T17:36:08Z",
-  "periodType": "TRIAL",
-  "cancelReason": "",
-  "environment": "PRODUCTION",
-  "isActive": true,
-  "stripeSubscription": "",
-  "IsTrialConversion": false,
-  "price": 0,
-  "currency": "USD",
-  "priceInPurchasedCurrency": 0,
-  "takehomePercentage": 0.7,
-  "transactionID": "190001314300675",
-  "originalTransactionID": "190001314300675",
-  "isFamilyShare": false
-}
-
+obj.subscriber.entitlements = {
+      "pro":{
+              "expires_date":"2029-05-26T05:05:04Z",
+              "product_identifier":"com.background.pro.yearly",
+              "purchase_date":"2022-04-09T05:05:04Z"
+      }
+  },
+  
+obj.subscriber.subscriptions ={
+      "com.background.pro.yearly":{
+              "billing_issues_detected_at":null,
+              "expires_date":"2029-05-26T05:05:04Z",
+              "is_sandbox":false,
+              "original_purchase_date":"2022-04-09T05:05:04Z",
+              "period_type":"trial",
+              "purchase_date":"2022-04-09T05:05:04Z",
+              "store":"app_store",
+              "unsubscribe_detected_at":null
+      }
+  }
 
 body = JSON.stringify(obj);
 $done({body});
