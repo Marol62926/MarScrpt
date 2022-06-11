83272d508a02899da282894ce3d2f55329a24a63
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-06-11 13:53:00 +0800

    Update mobileFax.js

--- mobileFax.js
+++ mobileFax.js
@@ -4,5 +4,5 @@ var obj = JSON.parse(body);
 obj.result.isActive = true,
 obj.result.expirationTime = "2029-04-27T18:05:01.294754Z",
 
-body = JSON.stringify(obj); 
-$done(body); 
+body = JSON.stringify(obj);
+$done({body});
