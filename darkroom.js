2c1ddd0a72a6b930b9b0bb9e49c743bdcb0665c9
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2022-07-08 13:22:51 +0800

    Update darkroom.js

--- darkroom.js
+++ darkroom.js
@@ -1,36 +1,36 @@
 var body = $response.body;
 var obj = JSON.parse(body);
 
-obj.subscriber = {
-    "entitlements": {
+obj.subscriber.entitlements = {
       "co.bergen.Darkroom.entitlement.allToolsAndFilters": {
-        "expires_date": "2029-05-02T12:48:25Z",
+        "expires_date": "2029-07-15T05:14:16Z",
         "grace_period_expires_date": null,
         "product_identifier": "co.bergen.Darkroom.product.year.everything",
         "purchase_date": "2022-07-08T05:14:16Z"
       },
       "co.bergen.Darkroom.entitlement.selectiveAdjustments": {
-        "expires_date": "2029-05-02T12:48:25Z",
+        "expires_date": "2029-07-15T05:14:16Z",
         "grace_period_expires_date": null,
         "product_identifier": "co.bergen.Darkroom.product.year.everything",
         "purchase_date": "2022-07-08T05:14:16Z"
       }
     },
   
-obj.subscriber.subscriptions ={
-      "co.bergen.Darkroom.product.year.everything":{
-              "billing_issues_detected_at": null,
-              "expires_date": "2029-05-02T12:48:25Z",
-              "grace_period_expires_date": null,
-              "is_sandbox": false,
-              "original_purchase_date": "2022-04-25T12:48:26Z",
-              "ownership_type": "PURCHASED",
-              "period_type": "normal",
-              "purchase_date": "2022-04-25T12:48:25Z",
-              "store": "app_store",
-              "unsubscribe_detected_at": null
+obj.subscriber.subscriptions = {
+      "co.bergen.Darkroom.product.year.everything": {
+        "billing_issues_detected_at": null,
+        "expires_date": "2029-07-15T05:14:16Z",
+        "grace_period_expires_date": null,
+        "is_sandbox": false,
+        "original_purchase_date": "2022-07-08T05:14:17Z",
+        "ownership_type": "PURCHASED",
+        "period_type": "normal",
+        "purchase_date": "2022-07-08T05:14:16Z",
+        "store": "app_store",
+        "unsubscribe_detected_at": null
       }
-  }
+    }
+
 
 body = JSON.stringify(obj); 
 $done({body});
