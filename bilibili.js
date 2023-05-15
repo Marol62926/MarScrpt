5d4dbadf8d23e56fd37e19790ee868bdd56ed97b
Marol62926 <65009425+Marol62926@users.noreply.github.com>
2023-05-16 00:17:13 +0800

    Update bilibili.js

--- bilibili.js
+++ bilibili.js
@@ -65,40 +65,5 @@ if (-1 != $request.url.indexOf('/x/v2/reply/main') && 0 == body['code']) {
     body['data']['notice'] = {};
 }
 
-// tab
-if (-1 != $request.url.indexOf('resource/show/tab?') && 0 == body['code']) {
-    // 会员购
-    body['data']['bottom'] = body['data']['bottom'].filter(function (item) {
-        return item.id != 180
-    });
-    // 70 直播 影视
-    body['data']['tab'] = body['data']['tab'].filter(function (item) {
-        return item.id != 38247 && item.id != 39 && item.id != 165
-    });
-    // 游戏中心
-    body['data']['top'] = body['data']['top'].filter(function (item) {
-        return item.id != 175
-    });
-}
-
-// 我的
-if (-1 != $request.url.indexOf('/x/v2/account/mine') && 0 == body['code']) {
-    body['data']['vip_section'] = {};
-    body['data']['vip_section_v2'] = {};
-    body['data']['sections'] = body['data']['sections'].filter(
-        function (item, index) {
-            if ('创作中心' == item.title) {
-                return true;
-            }
-            item.items = item.items.filter(function (section_items) {
-                console.log(section_items.title);
-                if ((['我的课程', '我的钱包', '会员购中心', '直播中心', '青少年模式', '看视频免流量', '我的客服'].includes(section_items.title))) {
-                    return true;
-                }
-                return true;
-            });
-            return true;
-        });
-}
 body = JSON.stringify(body);
 $done({body});
