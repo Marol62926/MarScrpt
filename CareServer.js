1e3a3c7523d111465fa8c294de9cc148fd1e30c4
Marol Lee <65009425+Marol62926@users.noreply.github.com>
2024-06-25 23:01:11 +0800

    Update CareServer.js

--- CareServer.js
+++ CareServer.js
@@ -6,17 +6,17 @@ obj.subscriber.entitlements = {
         "grace_period_expires_date" : null,
         "purchase_date" : "2024-04-25T14:41:50Z",
         "product_identifier" : "ShellBoxKit.Year",
-        "expires_date" : "2034-07-09T14:41:50Z"
+        "expires_date" : "2029-07-09T14:41:50Z"
       }
     },
   
 obj.subscriber.subscriptions = {
       "ShellBoxKit.Year" : {
         "original_purchase_date" : "2024-05-25T14:41:51Z",
-        "expires_date" : "2034-07-09T14:41:50Z",
+        "expires_date" : "2029-07-09T14:41:50Z",
         "is_sandbox" : false,
         "refunded_at" : null,
-        "store_transaction_id" : "480001974440210",
+        "store_transaction_id" : "48000197444080",
         "unsubscribe_detected_at" : "2024-04-25T14:42:40Z",
         "grace_period_expires_date" : null,
         "period_type" : "normal",
