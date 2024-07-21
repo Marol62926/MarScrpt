3ac7413fbcf58e2ca3e4a2b9353bccc9e4df6cce
Marol Lee <65009425+Marol62926@users.noreply.github.com>
2024-07-21 23:43:52 +0800

    Update colorwidget.js

--- colorwidget.js
+++ colorwidget.js
@@ -3,13 +3,13 @@ var obj = JSON.parse($response.body);
 obj.subscriber.entitlements = {
       "pro":{
               "expires_date":"2029-05-26T05:05:04Z",
-              "product_identifier":"cw_1999_ly_3d0",
+              "product_identifier":"cw_1999_1y_3d0",
               "purchase_date":"2022-04-09T05:05:04Z"
       }
   },
   
 obj.subscriber.subscriptions ={
-      "cw_1999_ly_3d0":{
+      "cw_1999_1y_3d0":{
               "billing_issues_detected_at":null,
               "expires_date":"2029-05-26T05:05:04Z",
               "is_sandbox":false,
